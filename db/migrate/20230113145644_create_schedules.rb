377ee73d0dffd8cfd15517852c4e524e7a90db28
kotsaito <intel.55.kota@gmail.com>
2023-01-14 01:25:32 +0900

    ユーザーとカレンダー導入

---
+++ db/migrate/20230113145644_create_schedules.rb
@@ -0,0 +1,14 @@
+class CreateSchedules < ActiveRecord::Migration[6.0]
+  def change
+    create_table :schedules do |t|
+      t.datetime   :start_time, null: false
+      t.string     :title,      null: false
+      t.string     :place  
+      t.string     :person
+      t.text       :practice
+      t.text       :others
+      t.references :user,              null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
