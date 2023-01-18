2d58ff82263a6da6d7e8920e70a41415b7d779ff
kotsaito <intel.55.kota@gmail.com>
2023-01-18 12:58:03 +0900

    新規予定作成機能

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,24 @@
 class SchedulesController < ApplicationController
   def index
+    @schedules = Schedule.order('start_time DESC')
+  end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:title, :start_time, :place, :person, :practice, :others).merge(user_id: current_user.id)
   end
 end
