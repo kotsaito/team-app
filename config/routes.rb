cf0eedcee147c146a1a0994f9f6a77df8206ed95
kotsaito <intel.55.kota@gmail.com>
2023-02-04 01:51:02 +0900

    チャっト機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:new, :create, :destroy]
   end
   root to: "schedules#index"
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:index, :new, :create, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
 end
